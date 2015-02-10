38a101264df7159dbc149a313e244d6613532b2c
lpcinelli <lpcinelli@gmail.com>
2015-02-10 16:17:49 +0100

    work

---
+++ Album Photo/Pages/Models/PageModel1.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Album_Photo.Pages.Models
+{
+    /// <summary>
+    /// Interaction logic for PageModel1.xaml
+    /// </summary>
+
+    public partial class PageModel1 : Album_Photo.Pages.GenericPage
+    {
+        public PageModel1()
+        {
+            InitializeComponent();
+        }
+  
+     
+    }
+}
